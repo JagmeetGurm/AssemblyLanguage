58006c73ed832592786f73225ccfbe67a22ace90
JagmeetGurm <jagmeetgurm@yahoo.com>
2015-08-23 12:42:38 -0700

    input prompts

--- practiceTemperature.asm
+++ practiceTemperature.asm
@@ -6,10 +6,14 @@
 INCLUDE io.h
 .STACK 4096
 
-
+.DATA 
 ; variables for prompt and taking input 
 promptOne BYTE "Enter temperature in Celsius: ", 0
+promptTwo BYTE "Enter 2nd temperature in celsius: ", 0
+promptThree BYTE "Enter 3rd temp. in Celsius: ", 0
 celStr BYTE 40 DUP (?)
+celStr2 BYTE 40 DUP (?)
+celStr3 BYTE 40 DUP (?)
 
 
 ; output label and variable
@@ -17,3 +21,17 @@ labl BYTE "The temp in Farenheit is: ", 0
 farStr BYTE 11 DUP (?)
 
 ; variables for storing values
+celVar DWORD ?
+farVar DWORD ?
+
+
+
+;code for taking input value and converting to farenheit
+.CODE
+_MainProc PROC
+
+input promptOne, celStr, 40
+atod celStr
+mov celVar, eax
+
+
